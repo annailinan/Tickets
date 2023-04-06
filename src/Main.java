d0001232f2dfd14ad50821cd478ed4b281e855d9
Anna Li <Kristusha3@yandex.ru>
2023-04-06 21:59:11 +0300

    commit6

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        int rubles = 20 ; //за сколько рублей дается одна миля
-
-        int ticketPrice = 12500; // Цена билета
-        System.out.println ((ticketPrice / rubles) + " Получите миль в подарок");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
